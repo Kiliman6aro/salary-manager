dc1cdd5954e8023e6b185951d769985e9a8a8985
B.Pavel <kiliman6aro@gmail.com>
2024-05-25 13:22:32 +0300

    Add i18n to project

--- src/main/java/ua/pp/hophey/salarymanager/Main.java
+++ src/main/java/ua/pp/hophey/salarymanager/Main.java
@@ -6,12 +6,19 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        Locale locale = new Locale("ru");
+        ResourceBundle bundle = ResourceBundle.getBundle("i18n/messages", locale);
+        String greeting = bundle.getString("greeting");
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText(greeting);
         btn.setOnAction(event -> System.out.println("Hello World!"));
 
         StackPane root = new StackPane();
