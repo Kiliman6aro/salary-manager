b7f93b6735532a341d4bf033ceac7640ac80fd0a
B.Pavel <kiliman6aro@gmail.com>
2024-05-25 12:50:02 +0300

    Add JavaFx to project

--- src/main/java/ua/pp/hophey/salarymanager/Main.java
+++ src/main/java/ua/pp/hophey/salarymanager/Main.java
@@ -1,11 +1,30 @@
 package ua.pp.hophey.salarymanager;
 
-import ua.pp.hophey.greeting.UserGreeting;
-import ua.pp.hophey.greeting.WorldGreeting;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(event -> System.out.println("Hello World!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        stage.setTitle("Hello World!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        WorldGreeting.greet();
-        UserGreeting.greet("Pavel");
+        launch(args);
     }
 }
